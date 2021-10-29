5004bfdae7c66a61c546255c6110231719da0ba8
ariel uzal <arieluzal@gmail.com>
2021-10-28 23:13:15 -0700

    Update smoke.js

--- 3_Generate/test_03/smoke.js
+++ 3_Generate/test_03/smoke.js
@@ -1,10 +1,11 @@
 class Smoke{
     constructor(pos){
         this.position = pos.copy();
-        this.position.x += random(-10,10);
-        this.position.y += random(-10,10);
+        this.position.x += random(-5,5);
+        this.position.y += random(-5,5);
+        this.position.z += 3;
         this.maxDiam = random(10,30);
-        this.diam = 0;
+        this.diam = 5;
         this.velocity = random(0.3,1.2);
         this.opacity = 255;
         this.remove = false;
@@ -23,7 +24,7 @@ class Smoke{
     update(){
         this.position.z += this.velocity;
         if(this.diam < this.maxDiam){
-            this.diam += 0.2;
+            this.diam += 0.1;
         }else if(this.opacity > 0){
             this.opacity -= 5;
         }else{
